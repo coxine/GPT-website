92b090219dffadbe4a96f303863f388408c5ec15
coxine <59347549+coxine@users.noreply.github.com>
2023-12-10 16:23:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GPT-website
+
+- 感谢原作者 <https://github.com/Aniuyyds/ChatGPT-website>
---
+++ static/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
+    // gpt3.5 代理接口：https://open.aiproxy.xyz/v1/chat/completions
+    url: "https://api.openai.com/v1/chat/completions",
+    apiKey: ""  // openAi Base64 编码 apiKey
+};  
